adec28bee720483023901158303fc26b70830d94
Timur Siraev <siraev.tima91@gmail.com>
2026-02-21 10:14:19 +0500

    feat: finish lab-3

--- app/src/main/java/com/example/badroidtips/MainActivity.kt
+++ app/src/main/java/com/example/badroidtips/MainActivity.kt
@@ -1,20 +1,30 @@
 package com.example.badroidtips
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.RecyclerView
+import com.example.badroidtips.data.TipsList
+import com.example.badroidtips.ui.TipAdapter
+import com.example.badroidtips.ui.TipDetailActivity
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val tips = TipsList.getTips()
+        val rv: RecyclerView = findViewById(R.id.tipsRecycler)
+
+        rv.adapter = TipAdapter(tips) { tip ->
+            val i = Intent(this, TipDetailActivity::class.java).apply {
+                putExtra(TipDetailActivity.EXTRA_DAY, tip.day)
+                putExtra(TipDetailActivity.EXTRA_TITLE, getString(tip.titleRes))
+                putExtra(TipDetailActivity.EXTRA_FULL, getString(tip.fullTextRes))
+                putExtra(TipDetailActivity.EXTRA_IMAGE, tip.imageRes)
+            }
+            startActivity(i)
         }
     }
 }
